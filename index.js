c07a3b165c9f49afda47559553ba334a0c06d777
EstrellaDionis <EstrellaDionis@gmail.com>
2022-03-29 18:19:59 -0400

    added morgan to show logs in the console

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+app.use(morgan("tiny")); //logger middleware and there are many middlewares to morgan! check docs!
 
 app.get("/", (req, res) => {
   res.send("Hello im working");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("Hello im woof woof");
 });
 
 app.listen(9000, () => {
