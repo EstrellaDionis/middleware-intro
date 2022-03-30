507fe6c550883e693c1007df575316f96847829d
EstrellaDionis <EstrellaDionis@gmail.com>
2022-03-29 20:26:15 -0400

    using app.use to illustrate how next works

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+//app.use will be triggered by every request!
 app.use(morgan("tiny")); //logger middleware and there are many middlewares to morgan! check docs!
+
+app.use((req, res, next) => {
+  console.log("THIS IS MY FIRST MIDDLEWARE");
+  next();
+  console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()");
+});
+
+app.use((req, res, next) => {
+  console.log("THIS IS MY SECOND MIDDLEWARE");
+  next();
+  console.log("THIS IS MY SECOND MIDDLEWARE - AFTER CALLING NEXT()");
+});
+
+app.use((req, res, next) => {
+  console.log("THIS IS MY THIRD MIDDLEWARE");
+  next();
+  console.log("THIS IS MY THIRD MIDDLEWARE - AFTER CALLING NEXT()");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello im working");
