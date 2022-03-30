f3b0f3b2ce5f54cb9608fa5b3507ddd0072bdddb
EstrellaDionis <EstrellaDionis@gmail.com>
2022-03-29 20:47:14 -0400

    recreated what morgan is doing to get a better understanding of how middleware works and why its beneficial

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const morgan = require("morgan");
 //app.use will be triggered by every request!
 app.use(morgan("tiny")); //logger middleware and there are many middlewares to morgan! check docs!
 
+//recreating what morgan does as an example of middleware
+app.use((req, res, next) => {
+  req.requestTime = Date.now();
+  console.log(req.method.toUpperCase(), req.path);
+  next();
+});
+
 //Check this out in the console to see how next works
 app.use((req, res, next) => {
   console.log("THIS IS MY FIRST MIDDLEWARE");
@@ -27,6 +34,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/dogs", (req, res) => {
+  console.log(`REQUEST DATE: ${req.requestTime}`); //this is using req.requestTime function from app.use() which is triggered by all requests and because it loads first, this endpoint is allowed to use it.
   res.send("Hello im woof woof");
 });
 
